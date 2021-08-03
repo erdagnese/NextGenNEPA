fa8583201d6909f0e1236d5267596f1a335ea800
erdagnese <52808167+erdagnese@users.noreply.github.com>
2021-08-03 11:55:58 -0700

    revised the sources required

--- functions/making.phyloseq.obj.R
+++ functions/making.phyloseq.obj.R
@@ -1,15 +1,13 @@
 #make.phyloseq.obj.R usage make.phyloseq.obj.R(asv.mat,metadata,tax.table)
 
-make.phyloseq.obj <- function(asv.mat,metadata, tax.table) {
+make.phyloseq.obj <- function(asv.mat,metadata, tax.mat) {
  
   require(tidyverse)
   require(plyr)
   require(vegan)
   require(phyloseq)
   
-  source("asv.matrix.R")
-  source("tax.table.R")
-  
+
   ASV = otu_table(asv.mat, taxa_are_rows = T)
   TAX = tax_table(tax.mat)
   
