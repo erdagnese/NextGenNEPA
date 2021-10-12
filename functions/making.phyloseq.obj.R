3741081635f00dfd157a1cbcb57a077bb2021161
erdagnese <dagnese@uw.edu>
2021-10-11 17:41:37 -0700

    phyloseq script update

--- functions/making.phyloseq.obj.R
+++ functions/making.phyloseq.obj.R
@@ -13,7 +13,7 @@ make.phyloseq.obj <- function(asv.mat,metadata, tax.mat) {
   physeq = phyloseq(ASV,TAX)
   
   samples = sample_data(metadata)
-  sample_names(samples) <- metadata$sample_id
+  sample_names(samples) <- metadata$Sample_name
   #we either need line 17 or we need to change the sample_id to Sample_name in the metadata to match the dada2 output ASV.csv
   physeq = merge_phyloseq(physeq,samples)
   
